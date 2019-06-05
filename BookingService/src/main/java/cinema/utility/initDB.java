51a87cab646615e82bc4c7bcc79ecbb704db2c97
Jeroen Roovers <jroovers93@gmail.com>
2019-06-05 14:48:55 +0200

    Booking testdata

--- BookingService/src/main/java/cinema/utility/initDB.java
+++ BookingService/src/main/java/cinema/utility/initDB.java
@@ -1,8 +1,12 @@
 package cinema.utility;
 
+import cinema.model.dao.BookingDao;
+import cinema.model.domain.entity.Booking;
+import java.util.ArrayList;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.inject.Inject;
 
 /**
  *
@@ -12,9 +16,24 @@ import javax.ejb.Startup;
 @Singleton
 public class initDB {
 
+    @Inject
+    BookingDao dao;
+
     @PostConstruct
     public void init() {
-        
+        createBookings();
+    }
+
+    private void createBookings() {
+        Booking b = new Booking();
+        b.setLocation("Eindhoven");
+        b.setMovietitle("The Shawshank Redemption");
+        b.setShowId(5L);
+        b.setRoom("Zaal 1");
+        b.setSeats(new ArrayList<>());
+        b.getSeats().add(7);
+        b.getSeats().add(8);
+        dao.create(b);
     }
 
 }
