0ca15d2ec50e7d960b46dbb410032d86e5345cb0
Jeroen Roovers <jroovers93@gmail.com>
2019-06-05 21:01:11 +0200

    docker stack file

--- build-everything.bat
+++ build-everything.bat
@@ -3,3 +3,8 @@ call mvn clean package -f ./MovieService/pom.xml
 call mvn clean package -f ./TheatreService/pom.xml
 cd WebUI
 call ng build --prod
+cd ..
+call docker build -t cinema/bookingservice:latest ./BookingService/
+call docker build -t cinema/movieservice:latest ./MovieService/
+call docker build -t cinema/theatreservice:latest ./TheatreService/
+call docker build -t cinema/frontend:latest ./WebUI/
