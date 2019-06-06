6ec1707e67abe04e05c7ef4286f7dfdd9b46222b
Jeroen Roovers <jroovers93@gmail.com>
2019-06-06 11:43:45 +0200

    cloud

--- build-apps.bat
+++ build-apps.bat
@@ -1,5 +1,5 @@
 call mvn clean package -f ./BookingService/pom.xml
 call mvn clean package -f ./MovieService/pom.xml
 call mvn clean package -f ./TheatreService/pom.xml
-#cd WebUI
-#call ng build --prod
+cd WebUI
+call ng build --prod
